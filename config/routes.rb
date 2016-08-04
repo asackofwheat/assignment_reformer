c3975f1bcc9464400d2f8c64ae2ef49cd395deda
Mike Lee <asackofwheat@gmail.com>
2016-08-04 19:57:42 -0400

    finishing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, :only => [:new, :create, :edit, :update]
+  root "users#index"
+
+  resources :users, :only => [:index, :show, :new, :create, :edit, :update]
   # get '/users' => 'users/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
